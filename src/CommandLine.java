992419db088535d4a20d690c8c793f8966507c09
oliver <ogarrott@cnm.edu>
2024-05-20 10:42:01 -0600

    Basic iteration of command line args

---
+++ src/CommandLine.java
@@ -0,0 +1,13 @@
+public class CommandLine {
+
+    public static void main(String[] args) {
+
+        for (String arc : args) {
+
+            System.out.println(args.length);
+        
+        }
+        
+    }
+
+}
